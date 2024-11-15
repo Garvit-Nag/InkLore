755c3bbd91acf35a098989b0ac669e7422831aa5
Garvit <154075021+Garvit-Nag@users.noreply.github.com>
2024-11-15 22:00:31 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,16 +1,7 @@
-import path from 'path'
-import { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
-// Ensure you're importing the correct type from Next.js
 const nextConfig: NextConfig = {
-  webpack(config) {
-    // TypeScript understands the config here because it is typed by NextConfig
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, 'src') // Resolves '@' to 'src'
-    }
-    return config
-  }
-}
+  /* config options here */
+};
 
-export default nextConfig
+export default nextConfig;
